18f67a7615fc6c0b7b439b51a92e7ab73172c469
ChinonsoOkonna <favourchinonso07@gmail.com>
2022-06-25 08:15:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.write("FIRST JAVASCRIPT TASK");
+document.write("<br>");
+document.write("<br>");
+document.write("First Name : Chinonso" + "<br>" + "<br>"  + "Middle Name : Favour " + "<br>" + "<br>" + "Last Name : Okonna" + "<br>" + "<br>" + "Height : 5.1" + "<br>" + "<br>" + "Country : Nigeria");
